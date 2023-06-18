c94f3113fd852c8e78f67fee593f48f0e9028406
Rúben Fernandes <111603870+Hasgnard@users.noreply.github.com>
2023-06-18 21:50:07 +0100

    Delete GenProtEAs directory

--- GenProtEAs/data/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python /inter.py "$1"
--- GenProtEAs/generativeModels/gVAE/io.py
+++
@@ -1,5 +0,0 @@
-def output_fasta(names, seqs, filepath):
-    with open(filepath, 'w') as fout:
-        for name, seq in zip(names, seqs):
-            fout.write('>{}\n'.format(name))
-            fout.write(seq+'\n')
