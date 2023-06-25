03eb030a4e417891cdcb78e6111cbbce122adeec
Rúben Fernandes <111603870+Hasgnard@users.noreply.github.com>
2023-06-25 22:02:39 +0100

    Delete deep_protein_generation_master directory

--- deep_protein_generation_master/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup, find_packages
-
-#run in terminal: pip install -e .
-#to install all modules in this package
-setup(name='proteingeneration', version='1.0', packages=find_packages())
